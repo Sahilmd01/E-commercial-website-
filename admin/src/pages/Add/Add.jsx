3023dd0e8f8252dc2497221f03a072de8c33cd33
MD. SAHIL <codewithkinu@gmail.com>
2025-06-09 18:22:33 +0530

    add section bug fix

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -4,7 +4,6 @@ import { assets } from '../../assets/assets'
 import axios from "axios"
 import { toast } from 'react-toastify'
 
-
 const Add = ({url}) => {
 
     const [image,setImage] = useState(false);
